f9892d10bf6a15d53e7a391f0ccb78ca695d4a0c
Claire-kaki <kihwagaclaire@gmail.com>
2026-06-23 21:13:46 +0300

    deleted
    cleaned up files

--- week6/employee_system/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db_connect.php';
-session_destroy();
-header("Location: /BIT3208_Project/week6/employee_system/login.php");
-exit();
-?>
--- week6/library_system/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "studentdb");
-if (!$conn) {
-    die("Library Database Connection Failed: " . mysqli_connect_error());
-}
-?>
